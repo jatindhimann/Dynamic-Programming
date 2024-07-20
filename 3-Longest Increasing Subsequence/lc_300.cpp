6ff263b33593d9bb830fd52fe6e588bb479a192f
jatinn27 <jatin121dhiman@gmail.com>
2024-07-20 10:13:38 +0530

    3 LIS bottom up

--- 3-Longest Increasing Subsequence/lc_300.cpp
+++ 3-Longest Increasing Subsequence/lc_300.cpp
@@ -36,3 +36,28 @@ public:
         return solve(nums, -1, 0);
     }
 };
+
+// bottom up
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int len = 1;
+        vector<int> dp(nums.size(), 1);
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i])
+                {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    len = max(len, dp[i]);
+                }
+            }
+        }
+
+        return len;
+    }
+};
