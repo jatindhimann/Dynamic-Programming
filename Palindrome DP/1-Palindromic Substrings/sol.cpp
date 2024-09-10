d7586d35b6dc428677e57328ae3ac55dbb700da2
jatinn27 <jatin121dhiman@gmail.com>
2024-09-10 21:39:48 +0530

    Palindromic Substrings

--- Palindrome DP/1-Palindromic Substrings/sol.cpp
+++ Palindrome DP/1-Palindromic Substrings/sol.cpp
@@ -84,3 +84,44 @@ public:
         return count;
     }
 };
+
+
+// Blueprint
+class Solution
+{
+public:
+    int countSubstrings(string s)
+    {
+        int n = s.length();
+        int t[1001][1001];
+        int count = 0;
+
+        for (int L = 1; L <= n; L++)
+        {
+            for (int i = 0; i + L - 1 < n; i++)
+            {
+                int j = i + L - 1;
+
+                if (i == j)
+                {
+                    t[i][j] = true;
+                }
+                else if (i + 1 == j)
+                {
+                    t[i][j] = (s[i] == s[j]);
+                }
+                else
+                {
+                    t[i][j] = (s[i] == s[j]) && t[i + 1][j - 1];
+                }
+
+                if (t[i][j])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
